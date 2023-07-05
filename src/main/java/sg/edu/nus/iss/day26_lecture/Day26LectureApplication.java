af40f54b0d838336262e816d01d5eb02439de143
antsimch <anthonysimch@gmail.com>
2023-07-05 11:50:44 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day26_lecture/Day26LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day26_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day26LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day26LectureApplication.class, args);
+	}
+
+}
